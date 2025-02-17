6f45879f9a932ffd6cbb4dc7b2199f49e13ef0a6
olga stepanova <empireme@yandex.ru>
2025-02-17 10:51:01 +0300

    rename processing

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
 from typing import List, Dict, Any, Tuple
 
 
-def filter_by_state(list_logs: List[Dict[str, Any]]) -> Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]:
+def filter_by_state(transactions_by_state: list[dict[str, Any]]) -> Tuple[list[dict[str, Any]], list[dict[str, Any]]]:
     """Функция принимает список словарей и возвращает список с ключом выбранного значения"""
-    new_list_1 = []
-    new_list_2 = []
-    for i in list_logs:
+    new_list1 = []
+    new_list2 = []
+    for i in transactions_by_state:
         if i.get('state') == 'EXECUTED':
-            new_list_1.append(i)
+            new_list1.append(i)
         else:
-            new_list_2.append(i)
-    return new_list_1, new_list_2
+            new_list2.append(i)
+    return new_list1, new_list2
 
 
 list_1, list_2 = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -18,12 +18,11 @@ list_1, list_2 = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': 
                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
 print(list_1)
-print(list_2)
 
 
-def sort_by_date(data_logs: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(transactions_by_date: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Функция принимает список словарей и возвращает список, отсортированный по дате"""
-    return sorted(data_logs, key=lambda x: x["date"], reverse=reverse)
+    return sorted(transactions_by_date, key=lambda x: x["date"], reverse=reverse)
 
 
 list_1 = sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
